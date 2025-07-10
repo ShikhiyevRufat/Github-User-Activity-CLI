c0eaca2bdf76ac4765ff563c3cc8167a0a508e8b
Rufat Shikhiyev <shikhiyevrufat@gmail.com>
2025-07-10 12:38:31 +0400

    I create it fully

--- src/manage_data.py
+++ src/manage_data.py
@@ -3,6 +3,11 @@ import urllib.request
 from src.config import BASE_URL
 import json
 from src.utils.error_message import error_message
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATA_FILE = os.path.join(BASE_DIR, 'data', 'tasks.json')
+
 
 def fetch_user_Event(username):
     url = f"{BASE_URL}{username}/events"
@@ -11,7 +16,10 @@ def fetch_user_Event(username):
         try:
             if response.status == 200:
                 data = response.read()
-                return json.loads(data)
+                parsed_data = json.loads(data)
+                with open(DATA_FILE, "w") as file:
+                    json.dump(parsed_data, file, indent=4)
+                return parsed_data
             else:
                 return None
         except urllib.error.HTTPError as e:
